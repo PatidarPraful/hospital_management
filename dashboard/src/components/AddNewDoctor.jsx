a3af67a68fe7d32c59d4a7af9188d8b4af418203
PatidarPraful <114163014+PatidarPraful@users.noreply.github.com>
2024-09-25 19:23:39 +0530

    Update AddNewDoctor.jsx

--- dashboard/src/components/AddNewDoctor.jsx
+++ dashboard/src/components/AddNewDoctor.jsx
@@ -56,7 +56,7 @@ const AddNewDoctor = () => {
       formData.append("doctorDepartment", doctorDepartment);
       formData.append("docAvatar", docAvatar);
       await axios
-        .post("http://localhost:4000/api/v1/user/doctor/addnew", formData, {
+        .post("https://peaceful-sunburst-4249df.netlify.app/api/v1/user/doctor/addnew", formData, {
           withCredentials: true,
           headers: { "Content-Type": "multipart/form-data" },
         })
